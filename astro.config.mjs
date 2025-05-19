698245f5ab9fdc503e37aed1076cdaade4b99bce
edavico <edavico@vandals.uidaho.edu>
2025-05-19 18:30:36 +0000

    5/19 update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import remarkMath from "remark-math"
+import rehypeKatex from "rehype-katex"
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  markdown: {
+    syntaxHighlight: "prism",
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+  site: "https:www.macrokroma.com",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
